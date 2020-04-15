7b8395f3d62d00819bf50546a2c11082c7f1a8a5
davkim1030 <hyukim>
2020-04-15 20:55:18 +0900

    changed main can get filename by argument list

--- linked_list/main.c
+++ linked_list/main.c
@@ -1,16 +1,24 @@
+/*
+** linked list를 활용하고 테스트 해보는 프로그램
+*/
+
 #include "linked_list.h"
 
-int					main(void)
+int					main(int c, char *v[])
 {
 	t_list	*list;
 
-	list = init_list();
-	if (load_data(list, "mydata.txt") == 0)
+	if (c > 1)
 	{
-		printf("해당 파일이 없습니다.\n");
-		return (0);
+
+		list = init_list();
+		if (load_data(list, v[1]) == 0)
+		{
+			printf("해당 파일이 없습니다.\n");
+			return (0);
+		}
+		print_list(list);
+		select_menu(list);
 	}
-	print_list(list);
-	select_menu(list);
 	return (0);
 }
